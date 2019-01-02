ee647956472369671994a4a48c2636fc042da4a0
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-01-02 23:58:59 +0100

    Allow usage of SO_RESUEADDR by setting LWIP opt
    Telnet confirms that port 443 is open.

--- mbedtls-showcase/Inc/lwipopts.h
+++ mbedtls-showcase/Inc/lwipopts.h
@@ -147,6 +147,7 @@
 #define CHECKSUM_CHECK_ICMP6 0
 /*-----------------------------------------------------------------------------*/
 /* USER CODE BEGIN 1 */
+#define SO_REUSE 1
 
 /* USER CODE END 1 */
 
