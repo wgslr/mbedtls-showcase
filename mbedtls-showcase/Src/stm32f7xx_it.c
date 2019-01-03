0cfcc306191f7fd9c06f01d96ed59d95225a02cd
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-01-03 01:11:55 +0100

    Make xprintfs in interrupt handlers more generic

--- mbedtls-showcase/Src/stm32f7xx_it.c
+++ mbedtls-showcase/Src/stm32f7xx_it.c
@@ -69,7 +69,7 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-  printf("HardFault_Handler\r\n");
+  printf("%s\r\n", __FUNCTION__);
 
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
@@ -88,7 +88,7 @@ void HardFault_Handler(void)
 void MemManage_Handler(void)
 {
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
-  printf("MemManager_Handler\r\n");
+  printf("%s\r\n", __FUNCTION__);
 
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
@@ -107,7 +107,7 @@ void MemManage_Handler(void)
 void BusFault_Handler(void)
 {
   /* USER CODE BEGIN BusFault_IRQn 0 */
-  printf("BusFault_Handler\r\n");
+  printf("%s\r\n", __FUNCTION__);
 
   /* USER CODE END BusFault_IRQn 0 */
   while (1)
@@ -126,7 +126,7 @@ void BusFault_Handler(void)
 void UsageFault_Handler(void)
 {
   /* USER CODE BEGIN UsageFault_IRQn 0 */
-  printf("UsageFault_Handler\r\n");
+  printf("%s\r\n", __FUNCTION__);
 
   /* USER CODE END UsageFault_IRQn 0 */
   while (1)
@@ -145,7 +145,7 @@ void UsageFault_Handler(void)
 void DebugMon_Handler(void)
 {
   /* USER CODE BEGIN DebugMonitor_IRQn 0 */
-  printf("DebugMon_Handler\r\n");
+  printf("%s\r\n", __FUNCTION__);
 
   /* USER CODE END DebugMonitor_IRQn 0 */
   /* USER CODE BEGIN DebugMonitor_IRQn 1 */
